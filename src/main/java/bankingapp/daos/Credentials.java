75ba73c3587e4f860729002e3e800b5ae5839352
Matt Keran <mattkeran@gmail.com>
2021-02-07 16:22:39 -0600

    current version

--- src/main/java/bankingapp/daos/Credentials.java
+++ src/main/java/bankingapp/daos/Credentials.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 public final class Credentials {
 
 	public static Connection getConnection() throws SQLException {
-		Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost/BankingApp", "postgres", "Eleven28Ten");
-		return conn;
+		return DriverManager.getConnection("jdbc:postgresql://localhost/BankingApp", "postgres", "Eleven28Ten");
 		}
 }
